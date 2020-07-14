b14f6750515ab7a34fb0db452ae6b4620cdf36a6
goto <gotomsak@gmail.com>
2020-07-15 05:21:46 +0900

    GetQuestionIDsの実装

---
+++ utils.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/jinzhu/gorm"
+)
+
+type Question struct {
+	gorm.Model
+	Question    string `json:"question"`
+	QimgPath    string `json:"qimg_path"`
+	Mistake1    string `json:"mistake1"`
+	Mistake2    string `json:"mistake2"`
+	Mistake3    string `json:"mistake3"`
+	Ans         string `json:"ans"`
+	MimgPath1   string `json:"mimg_path1"`
+	MimgPath2   string `json:"mimg_path2"`
+	MimgPath3   string `json:"mimg_path3"`
+	AimgPath    string `json:"aimg_path"`
+	Season      string `json:"season"`
+	QuestionNum string `json:"question_num"`
+	Genre       string `json:"genre"`
+}
+
+func sqlConnect() (database *gorm.DB) {
+	DBMS := os.Getenv("DBMS")
+	USER := os.Getenv("USERR")
+	PASS := os.Getenv("PASS")
+	PROTOCOL := os.Getenv("PROTOCOL")
+	DBNAME := os.Getenv("DBNAME")
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+	db, err := gorm.Open(DBMS, CONNECT)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+func remove(ints []int, search int) []int {
+	result := []int{}
+	for _, v := range ints {
+		if v != search {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+func searchIDs(ints []int, search int) bool {
+	for _, v := range ints {
+		if v == search {
+			return true
+		}
+	}
+	return false
+}
+
+func strToIntList(str string) []int {
+	intList := []int{}
+	str = strings.Trim(str, "[]")
+	strList := strings.Split(str, ",")
+	fmt.Println(strList)
+	if str != "" {
+		for i := 0; i < len(strList); i++ {
+			n, _ := strconv.Atoi(strList[i])
+			intList = append(intList, n)
+		}
+	}
+	return intList
+}
+
+// func shuffle(a []string) {
+// 	rand.Seed(time.Now().UnixNano())
+// 	for i := range a {
+// 		j := rand.Intn(i + 1)
+// 		a[i], a[j] = a[j], a[i]
+// 	}
+// }
