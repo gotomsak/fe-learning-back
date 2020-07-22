bed842f77b7779f77bcac0e921ce1dd66f9f1057
goto <gotomsak@gmail.com>
2020-07-22 14:01:23 +0900

    lintに怒られてたコメント，変数宣言を訂正

--- getQuestion.go
+++ getQuestion.go
@@ -13,9 +13,9 @@ func getQuestion(c echo.Context) error {
 	defer db.Close()
 	question := Question{}
 	questionSend := QuestionSend{}
-	questionId := c.QueryParam("id")
-	questionIdI, _ := strconv.Atoi(questionId)
-	db.First(&question, questionIdI)
+	questionID := c.QueryParam("id")
+	questionIDI, _ := strconv.Atoi(questionID)
+	db.First(&question, questionIDI)
 	qimg := regexp.MustCompile(",").Split(question.QimgPath, -1)
 	ans := []string{question.Ans, question.Mistake1, question.Mistake2, question.Mistake3}
 	aimg := []string{question.AimgPath, question.MimgPath1, question.MimgPath2, question.MimgPath3}
