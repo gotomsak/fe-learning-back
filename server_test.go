c4d04ba26f87fe1b36edda00d475cb40bbabb181
goto <gotomsak@gmail.com>
2020-07-26 05:25:24 +0900

    cookieを含むテストを実装

--- server_test.go
+++ server_test.go
@@ -11,31 +11,14 @@ import (
 	"github.com/labstack/echo"
 )
 
-func TestGetQuestion(t *testing.T) {
-	envLoad()
+var cookie *http.Cookie
 
-	e := router()
-	req := httptest.NewRequest("GET", "/question?id=6000", nil)
-
-	rec := httptest.NewRecorder()
-
-	e.ServeHTTP(rec, req)
-	log.Print(rec.Body)
-}
-
-func TestGetQusetionIDs(t *testing.T) {
-	envLoad()
-	e := router()
-	values := url.Values{}
-	values.Set("solved_ids", "[]")
-	values.Set("question_ids", "[]")
-	body := strings.NewReader(values.Encode())
-	log.Print(body)
-	req := httptest.NewRequest("POST", "/question_ids", body)
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationForm)
-	rec := httptest.NewRecorder()
-	e.ServeHTTP(rec, req)
-	log.Print(rec.Body)
+func parseCookies(value string) map[string]*http.Cookie {
+	m := map[string]*http.Cookie{}
+	for _, c := range (&http.Request{Header: http.Header{"Cookie": {value}}}).Cookies() {
+		m[c.Name] = c
+	}
+	return m
 }
 
 func TestSignup(t *testing.T) {
@@ -47,13 +30,13 @@ func TestSignup(t *testing.T) {
 	values.Set("username", "hoge")
 	values.Set("password", "foobar")
 	values.Set("email", "example.com")
-	// values.Set("test", "true")
 
 	body := strings.NewReader(values.Encode())
 	req := httptest.NewRequest(http.MethodPost, "/signup", body)
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationForm)
 	rec := httptest.NewRecorder()
 	e.ServeHTTP(rec, req)
+
 	log.Print(rec.Body)
 }
 
@@ -70,5 +53,36 @@ func TestSignin(t *testing.T) {
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationForm)
 	rec := httptest.NewRecorder()
 	e.ServeHTTP(rec, req)
+	cookie = parseCookies(rec.Header().Get("Set-Cookie"))["session"]
+
 	log.Print(rec.HeaderMap)
+}
+
+func TestGetQuestion(t *testing.T) {
+	envLoad()
+
+	e := router()
+	req := httptest.NewRequest("GET", "/question?id=6000", nil)
+
+	rec := httptest.NewRecorder()
+	req.Header.Add("Cookie", cookie.Name+"="+cookie.Value)
+
+	e.ServeHTTP(rec, req)
+	log.Print(rec.Body)
+}
+
+func TestGetQusetionIDs(t *testing.T) {
+	envLoad()
+	e := router()
+	values := url.Values{}
+	values.Set("solved_ids", "[]")
+	values.Set("question_ids", "[]")
+	body := strings.NewReader(values.Encode())
+	log.Print(body)
+	req := httptest.NewRequest("POST", "/question_ids", body)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationForm)
+	req.Header.Add("Cookie", cookie.Name+"="+cookie.Value)
+	rec := httptest.NewRecorder()
+	e.ServeHTTP(rec, req)
+	log.Print(rec.Body)
 }
